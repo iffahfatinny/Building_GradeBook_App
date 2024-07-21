d9fe29e8584d2e35f39b2149990e647abce39de7
Iffah Fatinny <iffah29@gmail.com>
2024-07-22 01:09:16 +0800

    gradebook

--- Building-Gradebook-App.js
+++ Building-Gradebook-App.js
@@ -47,4 +47,4 @@ function studentMsg(totalScores, studentScore) {
     );
   }
 }
-console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 40));
